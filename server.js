bffbf1169fd41907a34778786c32a9fe098d2f19
Kocsis Kevin <kocsis.kevin@hwstudio.hu>
2021-03-29 11:04:04 +0200

    ADDED: cors package, to allow all cors request for the endpoints

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
+var cors = require('cors')
 const data = require('./index')
 
 const api = express()
+
+//Allow all cors requests
+api.use(cors())
 
 const HOST = 'localhost'
 const PORT = 8888
